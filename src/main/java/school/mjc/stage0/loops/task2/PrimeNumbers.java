9c25a370897554daf23d37adeae34617ce25bf4d
Nadejda <nadyash295923@gmail.com>
2024-04-11 20:24:55 +0300

    done PN

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+
+        int i = 1;
+        int k = 1; //variable
+        int n = 0; // amount og dividers
+        while(i <= printToInclusive)
+        {
+            while(k != i)
+            {
+                if (i % k == 0 )
+                {
+                    n++;
+                }
+                k++;
+            }
+            if (n == 2) System.out.println(i);
+            i++; n = 0; k = 1;
+        }
     }
 }
