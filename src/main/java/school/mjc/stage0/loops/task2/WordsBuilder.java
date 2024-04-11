6851d917e157ad5feb5f12b559f99557f41afbbe
Nadejda <nadyash295923@gmail.com>
2024-04-11 21:05:21 +0300

    done WB

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        char whitespace =' ';int i = 0;
+        while ( i <= chars.length)
+        {
+            while (chars[i] != whitespace) {
+                System.out.print(chars[i++]);
+
+            }
+
+            i++;
+            System.out.print(whitespace);
+        }
+
 
     }
 }
