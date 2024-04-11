413a207990b40307ca0b87e99c22af28df9af6e6
Nadejda <nadyash295923@gmail.com>
2024-04-11 20:33:55 +0300

    done FN

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int i = 1;
+        int k = 1;
+        int result = 1;
+        if (printToInclusive == 0) System.out.println(1);
+        else {
+            System.out.println(1);
+            while (i <= printToInclusive) {
+                while (k <= i) {
+                    result *= k;
+                    k++;
+                }
+                k = 1;
+                System.out.println(result);
+                i++; result = 1;
+            }
+        }
     }
 }
